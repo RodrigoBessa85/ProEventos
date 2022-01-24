8d69fcf73859df8fb588465e60caa24bf0edf4b6
RodrigoBessa85 <rbessa_analista@hotmail.com>
2022-01-24 13:30:55 -0300

    first commit

---
+++ backend/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public EventoController()
+        {
+
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Value";
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "Value";
+        }
+
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return "Value";
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return "Value";
+        }
+    }
+}
