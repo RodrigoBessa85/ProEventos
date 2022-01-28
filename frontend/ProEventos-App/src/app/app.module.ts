cb3026a8f69800bc25b836bcfdab5731b327efc1
RodrigoBessa85 <rbessa_analista@hotmail.com>
2022-01-28 19:48:46 -0300

    Alterações e melhorias no layout

--- frontend/ProEventos-App/src/app/app.module.ts
+++ frontend/ProEventos-App/src/app/app.module.ts
@@ -1,24 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './templates/navbar/navbar.component';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 @NgModule({
   declarations: [
     AppComponent,
     EventosComponent,
-      PalestrantesComponent
+    PalestrantesComponent,
+    NavbarComponent
    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    CollapseModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/ProEventos-App/src/app/eventos/eventos.component.ts
+++ frontend/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+  widthImg = 150;
+  marginImg = 2;
+  exibirImg = true;
+  // tslint:disable-next-line: variable-name
+  private _filtroLista = '';
 
   constructor(private http: HttpClient) { }
 
@@ -19,10 +25,36 @@ export class EventosComponent implements OnInit {
   getEventos(): void {
     const url = 'https://localhost:5001/api/Evento';
     this.http.get(url).subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response,
+        this.eventosFiltrados = this.eventos
+      },
       error => console.log(error)
     );
   }
 
+  // tslint:disable-next-line: typedef
+  exibirImagem(){
+    this.exibirImg = !this.exibirImg;
+  }
+
+  public get filtroLista(): string {
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+
+    return this.eventos.filter(
+      (evento: any) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+
+    );
+  }
 
 }
