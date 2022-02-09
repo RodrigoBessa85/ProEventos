fe06400e2e2da525ee5100f514e3b48eb3482edd
RodrigoBessa85 <rbessa_analista@hotmail.com>
2022-02-09 03:24:19 -0300

    Adicionando o código dos formularios.

--- frontend/ProEventos-App/src/app/app-routing.module.ts
+++ frontend/ProEventos-App/src/app/app-routing.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { EventosComponent } from './components/eventos/eventos.component';
 import { PalestrantesComponent } from './components/palestrantes/palestrantes.component';
-import { ContatosComponent } from './components/contatos/contatos.component';
-import { PerfilComponent } from './components/perfil/perfil.component';
+
+import { EventosComponent } from './components/eventos/eventos.component';
 import { EventoDetalheComponent } from './components/eventos/evento-detalhe/evento-detalhe.component';
 import { EventoListaComponent } from './components/eventos/evento-lista/evento-lista.component';
+
+import { UserComponent } from './components/user/user.component';
+import { LoginComponent } from './components/user/login/login.component';
+import { RegistrationComponent } from './components/user/registration/registration.component';
+import { PerfilComponent } from './components/user/perfil/perfil.component';
+
+import { ContatosComponent } from './components/contatos/contatos.component';
+
+//import { AuthGuard } from './guard/auth.guard';
+//import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: '**', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'usuario', component: UserComponent,
+    children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'cadastrar', component: RegistrationComponent }
+    ]
+  },
+  { path: 'usuario/perfil', component: PerfilComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'eventos', redirectTo: 'eventos/lista' },
   { path: 'eventos', component: EventosComponent,
@@ -22,7 +39,6 @@ const routes: Routes = [
   },
   { path: 'palestrantes', component: PalestrantesComponent },
   { path: 'contatos', component: ContatosComponent },
-  { path: 'perfil', component: PerfilComponent },
 ];
 
 @NgModule({
--- frontend/ProEventos-App/src/app/shared/navbar/navbar.component.ts
+++ frontend/ProEventos-App/src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -9,9 +10,13 @@ export class NavbarComponent implements OnInit {
 
   isCollapsed = true;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+  }
+
+  exibirMenu(): boolean {
+    return this.router.url !== '/usuario/login';
   }
 
 }
